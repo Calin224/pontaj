13734bef2475c08de54385d7e14c6aefdde0d2b3
Calin224 <sanducalinm@gmail.com>
2025-03-31 10:09:25 +0300

    final_commit

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174e9f3efca026aa684252a0280dee898489347b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9412070c2cae08aefc696e5f64937101c2bbf12e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from './shared/home/home.component';
 import {authGuard} from './core/guards/auth.guard';
 import { RegisterComponent } from './features/account/register/register.component';
 import { PontajeComponent } from './features/pontaje/pontaje.component';
+import {ProfileComponent} from './features/account/profile/profile.component';
 
 export const routes: Routes = [
   {path: "", pathMatch: "full", component: HomeComponent},
   {path: "account/login", component: LoginComponent},
   {path: "account/register", component: RegisterComponent},
+  {path: "account/profile", component: ProfileComponent, canActivate: [authGuard]},
   {path: "pontaje", component: PontajeComponent, canActivate: [authGuard]}
 ];
